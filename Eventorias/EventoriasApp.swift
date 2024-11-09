0f052221e5a40f785834db09846e2dfb44fc379a
i-Louise <louisecindy.ta@gmail.com>
2024-11-09 01:56:38 +0100

    Initial Commit

---
+++ Eventorias/EventoriasApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventoriasApp.swift
+//  Eventorias
+//
+//  Created by Louise Ta on 09/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct EventoriasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
