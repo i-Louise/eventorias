1eed7eda469c8f0f44c1e67f95a79b52d21a7ac5
i-Louise <louisecindy.ta@gmail.com>
2024-11-22 17:39:00 +0100

    Added elements in UserProfileView

--- Eventorias/View/UserProfileView.swift
+++ Eventorias/View/UserProfileView.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct UserProfileView: View {
+    @State var notification = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("User profile")
+                        Spacer()
+                        Image(systemName: "person.crop.circle.fill")
+                    }
+                    VStack {
+                        Text("Name")
+                        Text("John Doe")
+                    }
+                    .padding(.horizontal, 15)
+                    .padding(.vertical, 15)
+                    .frame(maxWidth: .infinity)
+                    .background(.customGrey, in: RoundedRectangle(cornerRadius: 10))
+                    
+                    VStack {
+                        Text("E-mail")
+                        Text("John Doe")
+                    }
+                    .padding(.horizontal, 15)
+                    .padding(.vertical, 15)
+                    .frame(maxWidth: .infinity)
+                    .background(.customGrey, in: RoundedRectangle(cornerRadius: 10))
+                    
+                    Toggle("Notifications", isOn: $notification)
+                        .toggleStyle(SwitchToggleStyle(tint: .customRed))
+                }
+            }
+            .padding()
+            .background(Color.background)
+        }
     }
 }
 
